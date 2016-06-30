7280701a48f2be145ef6926983e77c0612ed7067
bradrn <bradn.neimann@hotmail.com>
2016-06-30 18:44:35 +1000

    Renamed AlgebraicTypes to CSFunc in project properties

--- CSFunc/Properties/AssemblyInfo.cs
+++ CSFunc/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AlgebraicTypes")]
+[assembly: AssemblyProduct("CSFunc")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
